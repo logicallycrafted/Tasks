534bf0d1cd41ed2b8c9cbe0fad58a1ac15e78cba
Rattan Ashat <rattanashat@logicallycrafted.com>
2023-12-25 00:27:33 -0500

    second commit

--- src/main/java/org/example/tasklist/User.java
+++ src/main/java/org/example/tasklist/User.java
@@ -15,4 +15,38 @@ public class User {
         setUserName (userName);
         setPhone (phone);
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * This is a username, cannot be blank
+     * @param userName -  whatever the user wants it to be
+     */
+    public void setUserName(String userName) {
+        userName =userName.trim(); //remove leading and trailing whitespaces
+        if (userName.matches("[A-z1-9]+"))
+
+        this.userName = userName;
+        else
+            throw new IllegalArgumentException("Username must have 1 char and/or 1 number");
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
+
